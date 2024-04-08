acf1d744a528ebf6d938e1022e684f02ace4e16f
yfantse <sevasti.yfanti@cloudform.lu>
2024-04-08 10:23:01 +0300

    fix nav to scroll with the rest of the header

--- src/js/scroll-header.js
+++ src/js/scroll-header.js
@@ -14,3 +14,20 @@ window.addEventListener('scroll', function () {
     blueDiv.style.position = 'relative'
   }
 })
+window.addEventListener('resize', function () {
+  //var nav = document.querySelector('.nav')
+  var euTenders = document.querySelector('.eu-tenders')
+  var nav = document.querySelector('.nav')
+  var panels = document.querySelector('.panels')
+
+  //var navHeight = parseFloat(window.getComputedStyle(nav).height)
+  var euTendersTop = parseFloat(window.getComputedStyle(euTenders).top)
+  // Calculate the remaining height of the viewport
+  var remainingHeight = window.innerHeight - euTendersTop + 18
+
+  // Now you can use remainingHeight for your needs
+  // For example, you can set it as a CSS variable
+  document.documentElement.style.setProperty('--nav-height--desktop', `${remainingHeight}px`)
+  nav.style.height = `${remainingHeight}px`
+  panels.style.height = `${remainingHeight}px`
+})
